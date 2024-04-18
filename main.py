85b1adcc25867afb656e35adf1ea360a9ab0294e
Andres Vega <andres.vega@sundevs.com>
2024-04-18 19:54:24 -0300

    mostrando la tortuga y disminuyendo la velocidad

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ t.pencolor("yellow")
 
 a = 0
 b = 0
-t.speed(0)
+t.speed(1)
 t.penup()
 t.goto(0, 200)
 t.pendown()
+t.showturtle()
 while(True):
     t.forward(a)
     t.right(b)
@@ -18,5 +19,5 @@ while(True):
     b +=1
     if b == 210:
         break
-    t.hideturtle()
+    
 turtle.done()
