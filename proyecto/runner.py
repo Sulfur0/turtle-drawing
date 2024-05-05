f630fdeea8baa455c75da2dafbb542aa4bc42c38
David Mendez Acuna <damenac@gmail.com>
2024-05-05 12:32:53 +0200

    Re-estructuracion de codigo

--- proyecto/runner.py
+++ proyecto/runner.py
@@ -1,64 +1,49 @@
 from canvas import Canvas
 from policy import PolicyIteration
 from logo import Logo
-import random
 
-rows, columns = 50, 50
+class Drawing:
 
-# Inicializo el tablero con todas las recompensas en cero
-board = [[' ' for _ in range(columns)] for _ in range(rows)]
+    def __init__(self):
+        self.rows, self.columns = 50, 50
 
-# Defino las recompensas de acuerdo a la figura que queremos dibujar.
-for i in range(rows):
-    for j in range(columns):
-        if i == 10 and j >= 10 and j <= 40:
-            board[i][j] = '+1'
-        if i == 40 and j >= 10 and j <= 40:
-            board[i][j] = '+1'
-        if j == 40 and i >= 10 and i <= 40:
-            board[i][j] = '+1'
-        if j == 10 and i >= 10 and i <= 40:
-            board[i][j] = '+1'
+        # Inicializo el tablero con todas las recompensas en cero
+        self.board = [[' ' for _ in range(self.columns)] for _ in range(self.rows)]
 
-board[0][0] = 'S'
-env = Canvas(board)
+        # Defino las recompensas de acuerdo a la figura que queremos dibujar.
+        for i in range(self.rows):
+            for j in range(self.columns):
+                if i == 10 and j >= 10 and j <= 40:
+                    self.board[i][j] = '+1'
+                if i == 40 and j >= 10 and j <= 40:
+                    self.board[i][j] = '+1'
+                if j == 40 and i >= 10 and i <= 40:
+                    self.board[i][j] = '+1'
+                if j == 10 and i >= 10 and i <= 40:
+                    self.board[i][j] = '+1'
 
-# Creación del agente
-agent = PolicyIteration(env)
-agent.policy_iteration()
+        self.board[0][0] = 'S'
+        self.env = Canvas(self.board)
 
-iterations = 30
-state = (0, 0)
-agent.mdp.initial_state = state
-agent.mdp.state = state
-pivot_state = state
+    
+    def train(self):
+        # Creo el agente obtengo la política que va a guiar la tortuga
+        # a lo largo del canvas en su tarea de dibujar.
+        agent = PolicyIteration(self.env)
+        agent.policy_iteration()
+        return agent
+    
 
-logo = Logo()
-last_action = None
+    def draw_policy(self, agent):
+        # Creo la tortuga que que recibe el agente y le pido que empiece
+        # el dibujo
+        logo = Logo(env=agent.mdp)
+        logo.draw(agent)
 
-for iteration in range(iterations):
-    policy = agent.policy[pivot_state[0]][pivot_state[1]]
 
-    if agent.mdp.actions_collide(policy, last_action):
-        pivot_state = (random.randrange(rows), random.randrange(columns))
-        logo.go_to(pivot_state)
-        agent.mdp.state = pivot_state
-        print('Collision. Jumping')
-        last_action = None
+    def run(self):
+        agent = self.train()
+        self.draw_policy(agent)
 
-    else:
-        if policy == 'down':
-            logo.down()
-        if policy == 'up':
-            logo.up()
-        if policy == 'left':
-            logo.left()
-        if policy == 'right':
-            logo.right()
 
-        last_action = policy
-        agent.mdp.do_action(policy)
-        pivot_state = agent.mdp.state
-
-    print(f'current position: pivot_state: {pivot_state}, mdp.state: {agent.mdp.state} run {policy}')
-
+Drawing().run()
