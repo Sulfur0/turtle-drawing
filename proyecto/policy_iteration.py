1c8f64b952133e286a461dc3f8dbf296dd1d84c6
David Mendez Acuna <damenac@gmail.com>
2024-05-11 22:35:14 +0200

    Mejora de la documentacion del codigo

--- proyecto/policy_iteration.py
+++ proyecto/policy_iteration.py
@@ -1,7 +1,7 @@
-import random
 from algorithm import Algorithm
-import warnings
 
+import random
+import warnings
 warnings.filterwarnings("ignore")
 
 class PolicyIteration(Algorithm):
@@ -11,13 +11,16 @@ class PolicyIteration(Algorithm):
         self.canvas = canvas
         self.discount = discount
         self.iterations = iterations
-        self.values = None
+        self.q_values = None
         self.policy = None
     
     
     def memoized_V(self, state):
+        '''
+        Retorna el valor calculado hasta el momento para el estado dado.
+        '''
         i, j = state
-        return self.values[i][j]
+        return self.q_values[i][j]
     
     
     def V(self, state, action):
@@ -61,43 +64,63 @@ class PolicyIteration(Algorithm):
                     self.canvas.state = state
 
                     # Tampoco queremos calcular los valores sobre los estados terminales
-                    # porque no queremos modificar su valor porque su valor inicial es
-                    # la recompensa que guía el algoritmo.
+                    # porque su valor inicial es la recompensa que guía el algoritmo.
                     if not self.canvas.is_terminal():
                         # Calculo la ecuación de Bellman para cada uno de los estados a los que puedo llegar
                         # con las acciones válidas desde el estado actual. 
                         action = self.policy[i][j]
-                        self.values[i][j] = self.V(state, action)
+                        self.q_values[i][j] = self.V(state, action)
                         self.canvas.state = state                   
                         
     
     def policy_improvement(self):
         '''
+        Éste método busca mejorar la política actual usando como insumo los q valores
+        actuales.
         '''
+
+        # Loop: Tenemos que recorrer todos los estados. En este caso, los estados son
+        #       las celdas de la matriz entonces entramos en un doble ciclo sobre los
+        #       índices i y j. 
         for i in range(self.canvas.nrows):
             for j in range(self.canvas.ncols):
                 state = (i, j)
+
+                # Es importante excluir los estados que representan obstáculos. Esos
+                # estados no tienen ni recompensa ni valor. Entonces, no nos interesa
+                # iterar sobre ellos. 
                 if self.canvas.values_board[i][j] != None:
                     self.canvas.state = state
+
+                    # Loop: Recorremos todas las acciones posibles a partir del estado
+                    #       actual. Seleccionamos la que mejor q_valor nos da a partir
+                    #       de los q_valores que hemos calculado hasta el momento.
                     best_action = self.policy[i][j]
-                    best_value = self.values[i][j]
+                    best_value = self.q_values[i][j]
                     for action in self.canvas.get_possible_actions(state):
                         new_value = self.V(state, action)
                         self.canvas.do_action(action)
                         if new_value > best_value:
                             best_value = new_value
+
+                            # No queremos modificar los q_valores sobre los estados terminales
+                            # porque su valor inicial es la recompensa que guía el algoritmo.
                             if not self.canvas.is_terminal(state=(i, j)):
-                                self.values[i][j] = new_value
+                                self.q_values[i][j] = new_value
                             best_action = action
                         self.canvas.state = state
                     self.policy[i][j] = best_action
 
     def policy_iteration(self):
+        '''
+        Éste método ejecuta el ciclo evaluación/mejora de la política a partir de
+        un criterio de convergencia.
+        '''
 
-        # Inicializamos los V arbitrariamente. Esta inicialización se decide
+        # Inicializamos los q valores arbitrariamente. Esta inicialización se decide
         # en el momento de inicializar las recompensas. Entonces, para la iteración
         # de políticas, simplemente hacemos una copia de los valores en el canvas.
-        self.values = [[self.canvas.values_board[i][j] for j in range(self.canvas.ncols)] for i in range(self.canvas.nrows)]
+        self.q_values = [[self.canvas.values_board[i][j] for j in range(self.canvas.ncols)] for i in range(self.canvas.nrows)]
 
         # Inicializamos la primera versión de la política (π) aleatoriamente. Naturalmente, 
         # no va a ser la política óptima pero será la política que empezaremos a 
@@ -119,8 +142,12 @@ class PolicyIteration(Algorithm):
         # Una vez terminado el ciclo evaluación/mejora, podemos actualizar el canvas
         # con los valores encontrados durante el algoritmo. Esto se hace simplemente
         # para poder visualizar los valores en el canvas.
-        self.canvas.values_board = [[self.values[i][j] for j in range(self.canvas.ncols)] for i in range(self.canvas.nrows)]
+        self.canvas.values_board = [[self.q_values[i][j] for j in range(self.canvas.ncols)] for i in range(self.canvas.nrows)]
 
 
     def run(self):
+        '''
+        Ejectuta el algoritmo. En este caso, lanza el algoritmo de 'policy_iteration'
+        que encuentra la mejor política.
+        '''
         self.policy_iteration()
