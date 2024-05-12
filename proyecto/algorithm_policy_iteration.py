9ff10de4f5e198cd8317ad2514c1fe7b6429fe72
David Mendez Acuna <damenac@gmail.com>
2024-05-12 13:22:28 +0200

    Herencia en los algoritmos

--- proyecto/algorithm_policy_iteration.py
+++ proyecto/algorithm_policy_iteration.py
@@ -10,40 +10,7 @@ logger = LoggerManager().getLogger()
 class PolicyIteration(AlgorithmImpl):
     
     def __init__(self, canvas, discount=0.9, iterations=500):
-        AlgorithmImpl.__init__(self)
-        self.discount = discount
-        self.iterations = iterations
-        self.q_values = None
-        self.policy = None
-        self.canvas = canvas
-
-    
-    def memoized_V(self, state):
-        '''
-        Retorna el valor calculado hasta el momento para el estado dado.
-        '''
-        i, j = state
-        return self.q_values[i][j]
-    
-    
-    def V(self, state, action):
-        '''
-        Calcula el nuevo valor para el estado 'state' si se ejecuta la acción 'action'.
-        Para esto, se usa la ecuación de Bellman.
-        '''
-        t = 1 # El ruido es 0 dado que el canvas es deterministico. Entonces t = 1 siempre
-        reward, state_prime = self.canvas.do_action(action)
-
-        # Si estoy en un estado terminal y me muevo a un estado terminal, entonces
-        # Jackspot! Esto lo hago para mostrarle a la tortuga que ya encontró el trazo
-        # y que entonces debe seguir avanzando por el mismo. 
-        if self.canvas.is_terminal(state) and self.canvas.is_terminal(state_prime):
-            return 2000
-        
-        # En todos los otros casos, aplico la ecuación de Bellman para calcular los valores
-        # que van a guiar a la tortuga a la primera casilla del trazo. 
-        return t * (self.canvas.values_board[state[0]][state[1]] + self.discount * self.memoized_V(state_prime))
-    
+        AlgorithmImpl.__init__(self, canvas, discount, iterations)
 
     
     def policy_evaluation(self):
